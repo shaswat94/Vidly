b6d99d6b3bf496b7d7ef78c5f1ccbb63b95bb0f9
Shaswat Swarup <shaswatswarup.94@gmail.com>
2018-03-13 12:05:26 +0530

    Initial Commit
    Committed the project of the first time

---
+++ Vidly/App_Start/RouteConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes();
+
+            //Custom Route
+            //routes.MapRoute(
+            //    "MovieByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { controller="Movies", action= "ByReleaseDate" },
+            //    //Custom Route constraint (only 2017 and 2018 considered for year and 2 digits for month)
+            //    new { year= @"2017|2018", month= @"\d{2}" }
+            //);
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext(); //disposable Db object that needs to be disposed properly
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ViewResult Index()
+        {
+            //Queries to the database to fetch the list of customers because of the presence of .ToList() function
+            //deferred execution
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            //Eager loading - Loads the Customer and its related properties here membership type.
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}
