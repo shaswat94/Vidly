10b4dd8d9cdb20b702eda8a4329802d1e9ac00c5
Shaswat Swarup <shaswatswarup.94@gmail.com>
2018-03-20 12:12:12 +0530

    Final CSS touch to the project
    --> Added Ellmah for exception logging
    --> Added a new file bootstrap-theme to change the look and feel of the website

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -27,6 +27,7 @@ namespace Vidly
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
+                      "~/Content/bootstrap-theme.css",
                       "~/Content/site.css",
                       "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/typeahead.css",
--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -17,8 +17,7 @@ namespace Vidly.Controllers
         public ActionResult About()
         {
 
-            //ViewBag.Message = "Your application description page.";
-            throw new Exception();
+            ViewBag.Message = "Your application description page.";
             return View();
         }
 
